9f85b5590bcdaa5640b78548f43e4e183e468bf4
shanmukhrvl <teja.ravella@gmail.com>
2020-12-03 17:32:57 +0530

    feat: input is processed and output is shown

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const btnTranslate = document.querySelector("#btn-translate");
 const textInput = document.querySelector("#text-input");
+const outputDiv = document.querySelector("#output-div");
 
 function inputTranslate() {
     const inputText = textInput.value;
-    console.log(inputText);
+    
+    outputDiv.innerText = inputText;
 }
 
 btnTranslate.addEventListener("click", inputTranslate);
