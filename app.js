0dfebba6ee5eaec958cbf0d99962020ac91e70cd
shanmukhrvl <teja.ravella@gmail.com>
2020-12-03 22:40:56 +0530

    feat: calls an api to translate input

--- app.js
+++ app.js
@@ -2,10 +2,28 @@ const btnTranslate = document.querySelector("#btn-translate");
 const textInput = document.querySelector("#text-input");
 const outputDiv = document.querySelector("#output-div");
 
+const serverURL = "https://api.funtranslations.com/translate/pirate.json";
+
+function getTranslatedURL(input) {
+    return serverURL + "?" + "text=" + input;
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("Something wrong with server, try again after some time");
+}
+
 function inputTranslate() {
     const inputText = textInput.value;
-    
-    outputDiv.innerText = inputText;
+
+    // calling server for processing
+    fetch(getTranslatedURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            const translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler);
 }
 
 btnTranslate.addEventListener("click", inputTranslate);
